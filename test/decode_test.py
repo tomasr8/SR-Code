87e26b7f06246b12ea79f6d54578df84cf064b0d
Tomas Roun <rountoma@fel.cvut.cz>
2022-07-30 22:23:09 +0200

    Fix tests

--- test/decode_test.py
+++ test/decode_test.py
@@ -5,25 +5,34 @@ import cv2
 from sr.decode import decode
 
 
+def _has_message(result, message):
+    return any(c.message.rstrip() == message for c in result.contours)
+
+
 def test_decode():
     data_dir = Path(__file__).parent / "data"
 
     image = cv2.imread(str(data_dir / "hello.png"))
-    result = decode(image)
-    assert result.message.rstrip() == "Hello world!"
+    result = decode(image, find_all=True, use_bw=True)
+    assert result.success
+    assert _has_message(result, "Hello world!")
 
     image = cv2.imread(str(data_dir / "hello1.jpg"))
-    result = decode(image)
-    assert result.message.rstrip() == "Hello world!"
+    result = decode(image, find_all=True, use_bw=True)
+    assert result.success
+    assert _has_message(result, "Hello world!")
 
     image = cv2.imread(str(data_dir / "hello2.jpg"))
-    result = decode(image)
-    assert result.message.rstrip() == "Hello world!"
+    result = decode(image, find_all=True, use_bw=True)
+    assert result.success
+    assert _has_message(result, "Hello world!")
 
     image = cv2.imread(str(data_dir / "hello3.jpg"))
-    result = decode(image)
-    assert result.message.rstrip() == "Hello world!"
+    result = decode(image, find_all=True, use_bw=True)
+    assert result.success
+    assert _has_message(result, "Hello world!")
 
     image = cv2.imread(str(data_dir / "empty.png"))
-    result = decode(image)
-    assert result.message.rstrip() == ""
+    result = decode(image, find_all=True, use_bw=True)
+    assert result.success
+    assert _has_message(result, "")
